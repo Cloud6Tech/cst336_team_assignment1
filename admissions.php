27d952a860e8a895b540f2ac381620466938bd3b
hmccabe <hmccabe@csumb.edu>
2016-01-31 21:11:23 -0800

    added user schools list functionality

--- admissions.php
+++ admissions.php
@@ -1,19 +1,22 @@
 <?php
+	session_start();	
+
 	// Open database connection
 	require 'db_connection.php';
 	
 	function getAdmissionsInfo() {
 		global $dbConn;
 		
-		$sql = "SELECT admissions_offices.phone, admissions_offices.website, public_universities.name 
-				FROM admissions_offices
+		$sql = "SELECT admission_offices.phone, admission_offices.website, public_universities.name 
+				FROM admission_offices
 				INNER JOIN public_universities
-				ON public_universities.public_university_id = admissions_offices.public_university_id
-				WHERE admissions_offices.public_university_id = :univId"; 
+				ON public_universities.public_university_id = admission_offices.public_university_id
+				WHERE admission_offices.public_university_id = :univId"; 
 		$stmt = $dbConn -> prepare($sql);
 		$stmt -> execute(array (":univId" => $_GET['id']));
 		return $stmt->fetch();
 	}
+	
 ?>
 
 <!DOCTYPE html>
@@ -42,6 +45,21 @@
 				<tr><th><?= $univInfo['name'] ?></th></tr>
 				<tr><td><?= $univInfo['phone'] ?></td></tr>
 				<tr><td><a href='<?= $univInfo['website'] ?>'><?= $univInfo['website'] ?></a></td></tr>
+				<tr><td>
+					<form method='post' action='updateList.php'>
+						<input type="hidden" name="redirect" value="admissions.php?id=<?=$_GET['id']?>">
+						<input type="hidden" name="univId" value=<?=$_GET['id']?>>
+						<?php
+							include 'updateList.php';
+							
+							if (inUserList($_GET['id'])) {
+								echo '<input type="submit" name="remove" value="Remove from My List">';
+							} else {
+								echo '<input type="submit" name="add" value="Add to My List">';
+							}
+					?>
+					</form>
+				</td></tr>
 			</table>
 			
 		</div>
