6b51094121146bb43ef903156e44bc3ab33d8247
hmccabe <hmccabe@csumb.edu>
2016-01-31 21:22:31 -0800

    fixing conflicts

--- admissions.php
+++ admissions.php
@@ -7,11 +7,11 @@
 	function getAdmissionsInfo() {
 		global $dbConn;
 		
-		$sql = "SELECT admission_offices.phone, admission_offices.website, public_universities.name 
-				FROM admission_offices
+		$sql = "SELECT admissions_offices.phone, admissions_offices.website, public_universities.name 
+				FROM admissions_offices
 				INNER JOIN public_universities
-				ON public_universities.public_university_id = admission_offices.public_university_id
-				WHERE admission_offices.public_university_id = :univId"; 
+				ON public_universities.public_university_id = admissions_offices.public_university_id
+				WHERE admissions_offices.public_university_id = :univId"; 
 		$stmt = $dbConn -> prepare($sql);
 		$stmt -> execute(array (":univId" => $_GET['id']));
 		return $stmt->fetch();
@@ -40,9 +40,8 @@
 				// Get university admissions info
 				$univInfo = getAdmissionsInfo();
 			?>
-			
 			<table class="admissionsInfo">
-				<tr><th><?= $univInfo['name'] ?></th></tr>
+				<tr><th><?= htmlentities($univInfo['name']) ?></th></tr>
 				<tr><td><?= $univInfo['phone'] ?></td></tr>
 				<tr><td><a href='<?= $univInfo['website'] ?>'><?= $univInfo['website'] ?></a></td></tr>
 				<tr><td>
